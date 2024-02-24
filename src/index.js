83b51eaf71c1269b1a873df6fa646bc68bedc00c
Djohan <d.roberto@student.unsw.edu.au>
2024-02-24 14:33:13 +0700

    delete App and updatae index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/home'
+import Gallery from './pages/Gallery/gallery'
+import Classes from './pages/Classes/classes'
+import Contact from './pages/Contact/contact'
+import About from './pages/About/about'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+     <Routes> 
+      <Route path='/' element={<Home/>}/>
+      <Route path='/gallery' element={<Gallery/>}/>
+      <Route path='/classes' element={<Classes/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
+     </Routes> 
+    </BrowserRouter> 
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
