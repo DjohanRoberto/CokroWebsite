4e9f7525bcbddf3b271af511e42be05ce27839e5
Djohan <d.roberto@student.unsw.edu.au>
2024-02-25 19:22:41 +0700

    changes to hero and rent section in home page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import './home.css'
 import NavBar from "../../component/navbar/navbar";
+
 import rentpic from "../../assets/kimchi.jpg"
+import heropic from "../../assets/messiBanner.jpg"
 
 const Home = () => {
     return <div className="Home">
@@ -13,7 +15,9 @@ const Home = () => {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                 </svg>
 
-                <div className="hero_img"></div>
+                <img className="hero_img" src={heropic} alt="" />
+
+                {/* <div className="hero_img"></div> */}
 
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="arrowicons">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
@@ -24,10 +28,10 @@ const Home = () => {
         <div className="rent">
             <div className="rent_left rent_section">
                 <h1>Rent Our Kitchen</h1>
-                <button>Button</button>
+                <button>Contact Us</button>
             </div>
             <div className="rent_mid rent_section">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, sed ad? Ad, sit minus nobis quia deserunt quas quos corrupti reprehenderit dicta ipsa minima ex omnis obcaecati impedit tenetur temporibus.</p>
+                <p>Our kitchen boasts a wide variety of high quality baking equipment, while also keeping a clean and professional look,.</p>
             </div>
             <div className="rent_right rent_section">
                 <img className="rent_pic"  src={rentpic} alt="" />
